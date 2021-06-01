fc023a78262ba6c2cc37af61a64114fe005eb474
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2021-06-01 22:26:17 +0200

    Branding, you know

--- MAIN/app/pages/_app.js
+++ MAIN/app/pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.scss";
 
-function MyApp({ Component, pageProps }) {
+function Ctrl({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default Ctrl;
