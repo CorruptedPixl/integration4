72d8517af4108c91a9ee6470f7aa9aba23022134
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2021-06-01 22:54:23 +0200

    Added image but won't load. .env.PUBLIC_URL returns undefined

--- MAIN/app/pages/index.js
+++ MAIN/app/pages/index.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
-import Parallax from "parallax-js";
+import ParallaxJS from "parallax-js";
+import { Parallax } from "react-parallax";
+
+// import landing from "/landing.png";
 
 export default function Home() {
   return (
@@ -16,6 +19,17 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <img href="./laptop.png"></img>
+
+        <Parallax
+          blur={10}
+          bgImage={process.env.PUBLIC_URL + "/laptop.png"}
+          bgImageAlt="the cat"
+          strength={200}
+        >
+          Content goes here. Parallax height grows with content height.
+        </Parallax>
 
         <p className={styles.description}>
           Lopem ipsum or something I don't know the actual text but there's
