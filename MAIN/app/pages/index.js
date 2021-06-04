fb1b57ae5a8a71d5d2cc1698d349a7a032a0b253
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2021-06-04 21:32:27 +0200

    Removed react mouse parallax in favor for react spring

--- MAIN/app/pages/index.js
+++ MAIN/app/pages/index.js
@@ -2,8 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import { Parallax } from "react-parallax"; // Scroll parallax
-import ParallaxMousemove from "react-parallax-mousemove"; // Mouse parallax
-import { useEffect, useRef } from "react";
 
 // import landing from "/landing.png";
 
@@ -16,41 +14,19 @@ export default function Home() {
         <link rel="icon" href="/ctrl.logo.svg" />
       </Head>
 
-      <main className={styles.main}>
+      <main className={styles.main__container}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <ParallaxMousemove
-        // containerStyle={styles.parallaxContainer}
-        // fullHeight={true}
+        <Parallax
+          blur={10}
+          bgImage="/laptop.png"
+          bgImageAlt="the cat"
+          strength={600}
         >
-          <Parallax
-            blur={10}
-            bgImage="/laptop.png"
-            bgImageAlt="the cat"
-            strength={600}
-          >
-            <ParallaxMousemove.Layer
-              layerStyle={styles.infoLayerStyle}
-              config={{
-                xFactor: 0.4,
-                yFactor: 0,
-                springSettings: {
-                  stiffness: 50,
-                  damping: 30,
-                },
-              }}
-            >
-              <Image
-                src="/laptop.png"
-                alt="laptop dude"
-                width={640}
-                height={496}
-              />
-            </ParallaxMousemove.Layer>
-            Content goes here. Parallax height grows with content height.
-            {/* <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
+          <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
+          Content goes here. Parallax height grows with content height.
           <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
           <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
           <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
@@ -58,9 +34,9 @@ export default function Home() {
           <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
           <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
           <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
-          <Image src="/laptop.png" alt="laptop dude" width={640} height={496} /> */}
-          </Parallax>
-        </ParallaxMousemove>
+          <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
+          <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
+        </Parallax>
 
         <p className={styles.description}>
           Lopem ipsum or something I don't know the actual text but there's
