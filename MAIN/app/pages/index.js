a8a3c8e4546c70c39daf36d9505150db16c8930d
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2021-06-04 22:21:51 +0200

    Got mouseMove parallax working with react spring! closes #39

--- MAIN/app/pages/index.js
+++ MAIN/app/pages/index.js
@@ -2,10 +2,26 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import { Parallax } from "react-parallax"; // Scroll parallax
+import { useSpring, animated } from "react-spring"; // Mouse parallax
 
 // import landing from "/landing.png";
 
 export default function Home() {
+  const calc = (x, y) => [
+    x - window.innerWidth / 2,
+    y - window.innerHeight / 2,
+  ];
+  const trans1 = (x, y) => `translate3d(${x / 10 / 10}rem,${y / 10 / 10}rem,0)`;
+  const trans2 = (x, y) => `translate3d(${x / 8 / 10}rem,${y / 8 / 10}rem,0)`;
+  const trans3 = (x, y) => `translate3d(${x / 6 / 10}rem,${y / 6 / 10}rem,0)`;
+  const trans4 = (x, y) =>
+    `translate3d(${x / 3.5 / 10}rem,${y / 3.5 / 10}rem,0)`;
+
+  const [props, set] = useSpring(() => ({
+    xy: [0, 0],
+    config: { mass: 10, tension: 550, friction: 140 },
+  }));
+
   return (
     <div className={styles.container}>
       <Head>
@@ -25,17 +41,97 @@ export default function Home() {
           bgImageAlt="the cat"
           strength={600}
         >
-          <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
-          Content goes here. Parallax height grows with content height.
-          <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
-          <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
-          <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
-          <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
-          <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
-          <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
-          <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
-          <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
-          <Image src="/laptop.png" alt="laptop dude" width={640} height={496} />
+          <div // Is necessary for mouseMove event to fire.
+            className={styles.parallaxContainer}
+            onMouseMove={({ clientX: x, clientY: y }) =>
+              set({ xy: calc(x, y) })
+            }
+          >
+            {/* This component MUST be in a container for the mouseMove event
+              to fire. The Parallax scroll bg does not register it */}
+            <animated.div style={{ transform: props.xy.to(trans1) }}>
+              <Image
+                class={styles.parallax__img}
+                src="/laptop.png"
+                alt="laptop dude"
+                width={640}
+                height={496}
+              />
+            </animated.div>
+            Content goes here. Parallax height grows with content height.
+            <animated.div style={{ transform: props.xy.to(trans2) }}>
+              <Image
+                src="/laptop.png"
+                alt="laptop dude"
+                width={640}
+                height={496}
+              />
+            </animated.div>
+            <animated.div style={{ transform: props.xy.to(trans3) }}>
+              <Image
+                src="/laptop.png"
+                alt="laptop dude"
+                width={640}
+                height={496}
+              />
+            </animated.div>
+            <animated.div style={{ transform: props.xy.to(trans4) }}>
+              <Image
+                src="/laptop.png"
+                alt="laptop dude"
+                width={640}
+                height={496}
+              />
+            </animated.div>
+            <animated.div style={{ transform: props.xy.to(trans1) }}>
+              <Image
+                src="/laptop.png"
+                alt="laptop dude"
+                width={640}
+                height={496}
+              />
+            </animated.div>
+            <animated.div style={{ transform: props.xy.to(trans2) }}>
+              <Image
+                src="/laptop.png"
+                alt="laptop dude"
+                width={640}
+                height={496}
+              />
+            </animated.div>
+            <animated.div style={{ transform: props.xy.to(trans3) }}>
+              <Image
+                src="/laptop.png"
+                alt="laptop dude"
+                width={640}
+                height={496}
+              />
+            </animated.div>
+            <animated.div style={{ transform: props.xy.to(trans4) }}>
+              <Image
+                src="/laptop.png"
+                alt="laptop dude"
+                width={640}
+                height={496}
+              />
+            </animated.div>
+            <animated.div style={{ transform: props.xy.to(trans2) }}>
+              <Image
+                src="/laptop.png"
+                alt="laptop dude"
+                width={640}
+                height={496}
+              />
+            </animated.div>
+            <animated.div style={{ transform: props.xy.to(trans3) }}>
+              <Image
+                src="/laptop.png"
+                alt="laptop dude"
+                width={640}
+                height={496}
+              />
+            </animated.div>
+          </div>
         </Parallax>
 
         <p className={styles.description}>
