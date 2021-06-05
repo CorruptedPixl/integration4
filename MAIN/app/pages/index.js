435e87bb30222ee8dfcd125393ce1b8ce3872c50
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2021-06-05 11:58:04 +0200

    fixed class -> classname

--- MAIN/app/pages/index.js
+++ MAIN/app/pages/index.js
@@ -11,11 +11,10 @@ export default function Home() {
     x - window.innerWidth / 2,
     y - window.innerHeight / 2,
   ];
-  const trans1 = (x, y) => `translate3d(${x / 10 / 10}rem,${y / 10 / 10}rem,0)`;
-  const trans2 = (x, y) => `translate3d(${x / 8 / 10}rem,${y / 8 / 10}rem,0)`;
-  const trans3 = (x, y) => `translate3d(${x / 6 / 10}rem,${y / 6 / 10}rem,0)`;
-  const trans4 = (x, y) =>
-    `translate3d(${x / 3.5 / 10}rem,${y / 3.5 / 10}rem,0)`;
+  const trans1 = (x, y) => `translate3d(${x / 10}rem,${y / 10}rem,0)`;
+  const trans2 = (x, y) => `translate3d(${x / 8}rem,${y / 8}rem,0)`;
+  const trans3 = (x, y) => `translate3d(${x / 6}rem,${y / 6}rem,0)`;
+  const trans4 = (x, y) => `translate3d(${x / 3.5}rem,${y / 3.5}rem,0)`;
 
   const [props, set] = useSpring(() => ({
     xy: [0, 0],
@@ -31,9 +30,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main__container}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>React Mouse Move Demo!</h1>
 
         <Parallax
           blur={10}
@@ -51,7 +48,7 @@ export default function Home() {
               to fire. The Parallax scroll bg does not register it */}
             <animated.div style={{ transform: props.xy.to(trans1) }}>
               <Image
-                class={styles.parallax__img}
+                className={styles.parallax__img}
                 src="/laptop.png"
                 alt="laptop dude"
                 width={640}
