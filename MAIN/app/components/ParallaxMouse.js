d5f0c3c3b941592509feb94690b0eb35deb06083
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2021-06-05 22:17:34 +0200

    Wrote the ParallaxMouse component. This wraps the animated.div in a neat component which takes a ton of optional props to make out code cleaner and our lives easier. Part 1/2 of #56

---
+++ MAIN/app/components/ParallaxMouse.js
@@ -0,0 +1,25 @@
+import { animated } from "react-spring";
+
+const ParallaxMouse = ({
+  xFactor, // Optional: Defaults to 5
+  yFactor, // Optional: Defaults to the same value as xFactor
+  springProps, // Required
+  className, // Optional
+  globalFactor, // Required
+  children, // Default prop
+}) => {
+  const transformFunc = (propsX, propsY) =>
+    `translate3d(${propsX / xFactor / globalFactor}rem,${propsY / (yFactor || xFactor) / globalFactor}rem,0)`;
+
+  return (
+    <animated.div style={{ transform: springProps.xy.to(transformFunc) }} className={className}>
+      {children /* Renders children (Images, Text, etc...)*/}
+    </animated.div>
+  );
+};
+
+ParallaxMouse.defaultProps = {
+  xFactor: 5,
+};
+
+export default ParallaxMouse;
