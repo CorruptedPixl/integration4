a58444f84f97bbe50af431e1b19a47b2f20e84a6
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2021-06-07 13:30:44 +0200

    fixed mousemovements for parallax closes #59

--- MAIN/app/pages/index.js
+++ MAIN/app/pages/index.js
@@ -30,82 +30,81 @@ export default function Home() {
         />
         <link rel="icon" href="/ctrl.logo.svg" />
       </Head>
-      <main className={styles.main__container}>
-        <section className={styles.main__container_intro}>
-          <h1 className={(styles.container__intro_title, styles.title)}>
-            Thought you were safe surfing <span className={styles.highlight}>the web?</span>
-          </h1>
-          <p className={styles.container__intro_text}>
-            Well you aren't. You're far from safe. Every move you make online is constantly being watched, tracked,
-            saved in a profile and used against you to not only show you targeted ads, but in the long term influence
-            the way you think.
-          </p>
-          <section className={styles.container__intro_buttons}>
-            <button className={styles.intro__buttons_button}>Experience how they do it</button>
-            <p className={styles.intro__buttons_or}>or</p>
-            <button className={styles.intro__buttons_button}>Learn to protect yourself</button>
+      <main className={styles.main__container} onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
+        <Parallax className={styles.main__container_introparallax} blur={10} strength={600}>
+          <section className={styles.main__container_intro}>
+            <h1 className={`${styles.title} ${styles.container__intro_title}`}>
+              Thought you were safe <br /> surfing <span className={styles.highlight}>the web?</span>
+            </h1>
+            <p className={styles.container__intro_text}>
+              Well you aren't. You're far from safe. Every move you make online is constantly being watched, tracked,
+              saved in a profile and used against you to not only show you targeted ads, but in the long term influence
+              the way you think.
+            </p>
+            <section className={styles.container__intro_buttons}>
+              <button className={styles.intro__buttons_button}>Experience how they do it</button>
+              <p className={styles.intro__buttons_or}>or</p>
+              <button className={styles.intro__buttons_button}>Learn to protect yourself</button>
+            </section>
           </section>
-        </section>
-        <Parallax blur={10} bgImage="/laptop.png" bgImageAlt="laptop dude" strength={600}>
-          <div // Is necessary for mouseMove event to fire for mouse parallax
+          {/*<div // Is necessary for mouseMove event to fire for mouse parallax
             className={styles.parallaxContainer}
-            onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}
-          >
-            {/* This component MUST be in a container for the mouseMove event
+          >*/}
+          {/* This component MUST be in a container for the mouseMove event
               to fire. The Parallax scroll bg does not register it */}
 
-            <ParallaxMouse xFactor={5} className={styles.parallax__img_surfer} springProps={springProps}>
-              <Image src="/surfer.png" alt="businessman on surfboard with briefcase" width={520} height={496} />
-            </ParallaxMouse>
+          <ParallaxMouse xFactor={10} className={styles.parallax__img_surfer} springProps={springProps}>
+            <Image src="/surfer.png" alt="businessman on surfboard with briefcase" width={520} height={496} />
+          </ParallaxMouse>
 
-            <ParallaxMouse xFactor={50} className={styles.parallax__img_bgWave} springProps={springProps}>
-              <Image src="/Smallwave.png" alt="smaller wave in the background" width={800} height={600} />
-            </ParallaxMouse>
+          <ParallaxMouse xFactor={50} className={styles.parallax__img_bgWave} springProps={springProps}>
+            <Image src="/Smallwave.png" alt="smaller wave in the background" width={800} height={600} />
+          </ParallaxMouse>
 
-            <ParallaxMouse xFactor={50} className={styles.parallax__img_mainWave} springProps={springProps}>
-              <div className={styles.parallax__img_fullwidth}>
-                {/* Wrapped this image in a div in order to extend outside the viewport width and still have parallax without ugly borders*/}
-                <Image
-                  className={styles.parallax__img}
-                  style={{
-                    overflow: "visible",
-                    minWidth: "150%",
-                    left: "-25%",
-                  }}
-                  src="/Bigwave.png"
-                  alt="Wave in the background where the surfer is surfing on"
-                  width={3000}
-                  height={800}
-                />
-              </div>
-            </ParallaxMouse>
-          </div>
+          <ParallaxMouse xFactor={50} className={styles.parallax__img_mainWave} springProps={springProps}>
+            <div className={styles.parallax__img_fullwidth}>
+              {/* Wrapped this image in a div in order to extend outside the viewport width and still have parallax without ugly borders*/}
+              <Image
+                className={styles.parallax__img}
+                style={{
+                  overflow: "visible",
+                  minWidth: "150%",
+                  left: "-25%",
+                }}
+                src="/Bigwave.png"
+                alt="Wave in the background where the surfer is surfing on"
+                width={3000}
+                height={800}
+              />
+            </div>
+          </ParallaxMouse>
+          {/*</Parallax></div>*/}
+          <section className={styles.main__container_become}>
+            <section className={styles.container__become_text}>
+              <h1 className={(styles.become__text_title, styles.title)}>
+                Become the eyes behind <span className={styles.highlight}>everything</span>
+              </h1>
+              <p className={styles.become__text_body}>
+                We've created an experience were we take you through the dark algoritm of the internet. We'll show you
+                what happens when you blindly accept cookies and you will decide Tom's fate.
+              </p>
+              <button className={styles.become__text_button}>Become big brother</button>
+            </section>
+            <img className={styles.container__become_img}></img>
+          </section>
+          <section className={styles.main__container_whatcanido}>
+            <img className={styles.container__whatcanido_img}></img>
+            <section className={styles.container__whatcanido_text}>
+              <h1 className={(styles.whatcanido__text_title, styles.title)}>What can I do?</h1>
+              <p className={styles.whatcanido__text_body}>
+                Now you might be wondering what you can do to prevent such activities. Don't worry we've listed
+                everything you can do and we created a tool to scan yourself in which you can see everything companies
+                can track about you when you accept cookies.
+              </p>
+              <button className={styles.whatcanido__text_button}>Become big brother</button>
+            </section>
+          </section>
         </Parallax>
-        <section className={styles.main__container_become}>
-          <section className={styles.container__become_text}>
-            <h1 className={(styles.become__text_title, styles.title)}>
-              Become the eyes behind <span className={styles.highlight}>everything</span>
-            </h1>
-            <p className={styles.become__text_body}>
-              We've created an experience were we take you through the dark algoritm of the internet. We'll show you
-              what happens when you blindly accept cookies and you will decide Tom's fate.
-            </p>
-            <button className={styles.become__text_button}>Become big brother</button>
-          </section>
-          <img className={styles.container__become_img}></img>
-        </section>
-        <section className={styles.main__container_whatcanido}>
-          <img className={styles.container__whatcanido_img}></img>
-          <section className={styles.container__whatcanido_text}>
-            <h1 className={(styles.whatcanido__text_title, styles.title)}>What can I do?</h1>
-            <p className={styles.whatcanido__text_body}>
-              Now you might be wondering what you can do to prevent such activities. Don't worry we've listed everything
-              you can do and we created a tool to scan yourself in which you can see everything companies can track
-              about you when you accept cookies.
-            </p>
-            <button className={styles.whatcanido__text_button}>Become big brother</button>
-          </section>
-        </section>
       </main>
     </div>
   );
