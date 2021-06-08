56ab936934b4a926f1b83176312f9e7e2a1b7ad4
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2021-06-08 19:03:19 +0200

    setup secureyourself page #64

---
+++ MAIN/app/pages/secureyourself.js
@@ -0,0 +1,15 @@
+import styles from "../styles/Secureyourself.module.scss";
+import Image from "next/image";
+import { Parallax } from "react-parallax"; // Scroll parallax
+import { useSpring } from "react-spring"; // Mouse parallax
+import { useState } from "react";
+import ThreejsObjects from "../components/ThreejsObjects.js";
+import ParallaxMouse from "../components/ParallaxMouse";
+
+export default function secureyourself() {
+  return (
+    <>
+      <h1>Secure yourself agains tracking!</h1>
+    </>
+  );
+}
