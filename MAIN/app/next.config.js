78e2f0f3206118c402d2e0657d19e35d941f4c05
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2021-06-10 20:37:09 +0200

    Fixed client side issue "fs not found" because it's client side, and not a server, so ofc fs isn't found smh. Anyways, disabled it in webpack so it works now.

---
+++ MAIN/app/next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Fixes packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+      };
+    }
+
+    return config;
+  },
+};
