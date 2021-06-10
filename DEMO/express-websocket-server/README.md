489c246d055a9486456dafa7125cf1167f17d674
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2021-06-10 12:02:05 +0200

    Added socket.io server for concole. Pushing to heroku #66

---
+++ DEMO/express-websocket-server/README.md
@@ -0,0 +1,28 @@
+# Socket.io Express backend
+
+This demo is our Node.js backend running Socket.io on Express to send data to and from all currently connected consoles.
+
+## Try it yourself
+
+To get a local copy up and running follow these simple steps.
+
+### Prerequisites
+
+We'll assume you have the latest node version and yarn installed already.
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/CorruptedPixl/integration4.git
+   ```
+2. Navigate to folder and install packages
+   ```sh
+   cd DEMO/express-websocket-server && yarn
+   ```
+3. Run this demo
+   ```sh
+   node .
+   ```
+
+You're good to go. Have fun!
---
+++ DEMO/express-websocket-server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  //   res.sendFile(__dirname + "/index.html");
+  res.status(400).send("Bad request. Only wss:// protocol supported.");
+});
+
+app.get("/console/external", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
