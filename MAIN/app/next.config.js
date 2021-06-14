1bd5ef88c904b05c76e7712fd86cf64b1d962512
Kenzo-dewaegenaere <71668365+Kenzo-dewaegenaere@users.noreply.github.com>
2021-06-14 13:51:43 +0200

    #74 texture fix

--- MAIN/app/next.config.js
+++ MAIN/app/next.config.js
@@ -7,6 +7,17 @@ module.exports = {
       };
     }
 
+    //fileloader test
+    /*
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif)$/i,
+      use: [
+        {
+          loader: "file-loader",
+        },
+      ],
+    });
+*/
     return config;
   },
 };
