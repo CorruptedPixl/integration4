73264bee85989fd32b0a0ec1dd6702d3b54f852e
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2021-06-22 12:51:29 +0200

    Added comments #109

--- MAIN/app/components/Toggle.js
+++ MAIN/app/components/Toggle.js
@@ -2,6 +2,7 @@ import styles from "../styles/Toggle.module.scss";
 
 const Toggle = ({ className, valueLeft, valueRight, toggleState, setToggleState }) => {
   const handleToggleState = () => {
+    // Switch between the values given, this enables use of [toggleState] as part of translation instead of returning a boolean here and figuring out which language it should be
     setToggleState(toggleState === valueLeft ? valueRight : valueLeft);
   };
 
