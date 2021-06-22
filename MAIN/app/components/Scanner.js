f4372f0ab9762bfe7e512e54dd7b9737f6de0955
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2021-06-22 23:09:43 +0200

    Added translation framework Closes #117 Closes #120

--- MAIN/app/components/Scanner.js
+++ MAIN/app/components/Scanner.js
@@ -1,12 +1,15 @@
-import styles from "../styles/Scanner.module.scss";
 import { useEffect, useState } from "react";
+import styles from "../styles/Scanner.module.scss";
+import translations from "../translations/scanner.json";
 import FingerprintJS from "@fingerprintjs/fingerprintjs-pro";
 
-const Scanner = ({ visible, setVisitorData }) => {
+const Scanner = ({ visible, setVisitorData, currentLang }) => {
   const [visitorInfo, setVisitorInfo] = useState();
   const [responseSummary, setResponseSummary] = useState();
   const [serverData, setServerData] = useState();
   const [lastVisit, setLastVisit] = useState();
+  const [weatherData, setWeatherData] = useState(null);
+  const [showAllVisits, setShowAllVisits] = useState(false);
 
   useEffect(() => {
     // Get the fingerprint
@@ -38,17 +41,64 @@ const Scanner = ({ visible, setVisitorData }) => {
         return response.json();
       })
       .then((data) => {
-        console.log(data.visits);
         setResponseSummary(
           <p>
-            You've visited this site{" "}
-            <code className={styles.code}>{data.visits.length >= 100 ? "100+" : data.visits.length}</code> times
-            already.
+            {translations.summary.visited[currentLang]}{" "}
+            <code className={styles.code}>{data.visits.length >= 100 ? "100+" : data.visits.length}</code>{" "}
+            {translations.summary.times[currentLang]}
           </p>
         );
         setServerData(data);
+        console.log(data);
         setLastVisit(data.visits[0]);
       });
+  };
+
+  useEffect(() => {
+    if (lastVisit) {
+      callWeatherAPI();
+    }
+  }, [lastVisit]);
+
+  const callWeatherAPI = () => {
+    // Get the user's past visits and data from fpjs servers
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lastVisit.ipLocation.latitude}&lon=${lastVisit.ipLocation.longitude}&appid=${process.env.NEXT_PUBLIC_WEATHER_APIKEY}&units=metric`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setWeatherData(data);
+      });
+  };
+
+  const getWeatherTempFeeling = () => {
+    const temp = weatherData.main.temp;
+    switch (true) {
+      case temp >= 30:
+        return "like it's burning";
+        break;
+      case temp >= 20:
+        return "pretty hot";
+        break;
+      case temp >= 15:
+        return "pretty average";
+        break;
+      case temp >= 10:
+        return "quite chilly";
+        break;
+      case temp < 10:
+        return "cold";
+        break;
+      case temp <= 0:
+        return "like it's freezing";
+        break;
+
+      default:
+        break;
+    }
   };
 
   // Send back data to parent component if asked for it
@@ -59,41 +109,48 @@ const Scanner = ({ visible, setVisitorData }) => {
       {serverData && lastVisit ? (
         <>
           {/* Would've used <Suspense/> but react dom doesn't support that right now, so we're doing it the old school way */}
-          <h3 className={styles.subtitle}>Right now, this is what we can track about you</h3>
+          <h3 className={styles.subtitle}>{translations.story.subtitle[currentLang]}</h3>
+
           <p>
-            You <em className={styles.emphasized}>unique</em> visitor id is:{" "}
+            {translations.story.p1[currentLang]}{" "}
+            {/* Error handling is present in case either fpjs can't find the location or the weather api fails*/}
+            {weatherData
+              ? translations.story.p1_weather_available[currentLang]
+              : translations.story.p1_weather_unavailable[currentLang]}{" "}
+            {weatherData && `${getWeatherTempFeeling()} ${translations.story.currently[currentLang]} `}
+            {weatherData && (
+              <code className={styles.code}>{weatherData && Math.round(weatherData.main.temp)}°C</code>
+            )}{" "}
+            {translations.story.down[currentLang]} {weatherData ? weatherData.name : lastVisit.ipLocation.city.name}
+          </p>
+          <p>{translations.story.live[currentLang]}</p>
+          {weatherData && (
+            <p>
+              {translations.story.coords[currentLang]}{" "}
+              <code className={styles.code}>Lat:{weatherData && weatherData.coord.lat}</code>{" "}
+              <code className={styles.code}>Long:{weatherData && weatherData.coord.lon}</code>
+            </p>
+          )}
+          <br />
+
+          <p>
+            {translations.story.id_you[currentLang]}
+            <em className={styles.emphasized}> {translations.story.id_unique[currentLang]} </em>
+            {translations.story.id_id[currentLang]}{" "}
             <code className={styles.code}>{visitorInfo && visitorInfo.visitorId}</code>
           </p>
           <p>
-            You're using {lastVisit.browserDetails.browserName}, version{" "}
-            <code className={styles.code}>{lastVisit.browserDetails.browserFullVersion}</code> on{" "}
-            <code className={styles.code}>
-              {lastVisit.browserDetails.os} {lastVisit.browserDetails.osVersion}.
-            </code>
+            {translations.story.ip_about[currentLang]} <code className={styles.code}>{serverData && lastVisit.ip}</code>{" "}
+            {translations.story.ip_about2[currentLang]}
           </p>
-          <p>
-            The probability that you're a robot is{" "}
-            <code className={styles.code}>{lastVisit.browserDetails.botProbability * 100} %</code>
-          </p>
-          {responseSummary}
-          <p>
-            {serverData && lastVisit.incognito
-              ? "You're using incognito right now (try without!)"
-              : "You're not using incognito right now (try it out!)"}
-          </p>
-          <p>
-            Your IP address is <code className={styles.code}>{serverData && lastVisit.ip}</code>
-          </p>
-
           {lastVisit.ipLocation.city.name && lastVisit.ipLocation.postalCode && lastVisit.ipLocation.country.name ? (
             <>
               <p>
-                Based on your IP, you're probably in or close to{" "}
+                {translations.story.ip_address[currentLang]}{" "}
                 <code className={styles.code}>
                   {lastVisit.ipLocation.city.name}, {lastVisit.ipLocation.postalCode},{" "}
                   {lastVisit.ipLocation.country.name}
                 </code>{" "}
-                right now.
               </p>
             </>
           ) : (
@@ -102,24 +159,60 @@ const Scanner = ({ visible, setVisitorData }) => {
               error, our apologies.
             </code>
           )}
+          <br />
 
-          <p>Feel free to copy and paste that next time you need to fill in your address online 😉</p>
+          <p>
+            {translations.story.browser_using[currentLang]} {lastVisit.browserDetails.browserName}
+            {translations.story.browser_v[currentLang]}{" "}
+            <code className={styles.code}>{lastVisit.browserDetails.browserFullVersion}</code>{" "}
+            {translations.story.browser_on[currentLang]}{" "}
+            <code className={styles.code}>
+              {lastVisit.browserDetails.os} {lastVisit.browserDetails.osVersion}
+            </code>
+            {", "}
+            {serverData && lastVisit.incognito
+              ? translations.story.incog_true[currentLang]
+              : translations.story.incog_false[currentLang]}
+          </p>
+          <p>
+            {translations.story.robot[currentLang]}{" "}
+            <code className={styles.code}>{lastVisit.browserDetails.botProbability * 100} %</code>
+          </p>
 
-          <p>Below you can see a list of the exact time you visited this site</p>
+          {responseSummary}
+          <p>{translations.story.visits_list[currentLang]}</p>
           <ul className={styles.visit__list}>
-            {serverData.visits.map((visit, index) => {
-              const visitTime = new Date(visit.timestamp);
-              return (
-                <li key={index}>
-                  {visitTime.getDate() === new Date().getDate()
-                    ? "Today at "
-                    : `${visitTime.getFullYear()}/${visitTime.getMonth()}/${visitTime.getDate()}`}{" "}
-                  {visitTime.getHours()}:{visitTime.getMinutes()}{" "}
-                  {visit.ipLocation.city.name && `from ${visit.ipLocation.city.name}`}
-                </li>
-              );
-            })}
+            {showAllVisits
+              ? serverData.visits.map((visit, index) => {
+                  const visitTime = new Date(visit.timestamp);
+                  return (
+                    <li key={index}>
+                      {visitTime.getDate() === new Date().getDate()
+                        ? translations.story.visits_today[currentLang]
+                        : `${visitTime.getFullYear()}/${visitTime.getMonth()}/${visitTime.getDate()}`}{" "}
+                      {visitTime.getHours()}:{visitTime.getMinutes()}{" "}
+                      {visit.ipLocation.city.name && `from ${visit.ipLocation.city.name}`}
+                    </li>
+                  );
+                })
+              : serverData.visits.slice(0, 5).map((visit, index) => {
+                  const visitTime = new Date(visit.timestamp);
+                  return (
+                    <li key={index}>
+                      {visitTime.getDate() === new Date().getDate()
+                        ? translations.story.visits_today[currentLang]
+                        : `${visitTime.getFullYear()}/${visitTime.getMonth()}/${visitTime.getDate()}`}{" "}
+                      {visitTime.getHours()}:{visitTime.getMinutes()}{" "}
+                      {visit.ipLocation.city.name && `from ${visit.ipLocation.city.name}`}
+                    </li>
+                  );
+                })}
           </ul>
+          <a className={styles.visitsButton} onClick={() => setShowAllVisits(!showAllVisits)}>
+            {showAllVisits
+              ? translations.story.visits_btn_hide[currentLang]
+              : translations.story.visits_btn[currentLang]}
+          </a>
         </>
       ) : (
         <>
