abefc384a8453c5ec68a0b34aeb28da766efc86e
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2021-06-22 13:05:25 +0200

    Cleaned up code, added comments #109

--- MAIN/app/hooks/useLocalStorage.js
+++ MAIN/app/hooks/useLocalStorage.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-// Hook
-function useLocalStorage(key, initialValue) {
+const useLocalStorage = (key, initialValue) => {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
     try {
+      // Prevent this hook from being executed server side and creating errors because window is undefined
       if (typeof window !== "undefined") {
         // Get from local storage by key
         const item = window.localStorage.getItem(key);
@@ -20,8 +20,7 @@ function useLocalStorage(key, initialValue) {
       return initialValue;
     }
   });
-  // Return a wrapped version of useState's setter function that ...
-  // ... persists the new value to localStorage.
+  // Return a wrapped version of useState's setter function that persists the new value to localStorage.
   const setValue = (value) => {
     try {
       // Allow value to be a function so we have same API as useState
@@ -31,10 +30,10 @@ function useLocalStorage(key, initialValue) {
       // Save to local storage
       window.localStorage.setItem(key, JSON.stringify(valueToStore));
     } catch (error) {
-      // A more advanced implementation would handle the error case
+      // A more advanced implementation would handle the error case (but this is a beta)
       console.log(error);
     }
   };
   return [storedValue, setValue];
-}
+};
 export default useLocalStorage;
