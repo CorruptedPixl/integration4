bb2fbda8d89392bfdb40d25dba4739be8cabb537
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2021-06-23 21:49:15 +0200

    removed helmet

--- MAIN/app/pages/index.js
+++ MAIN/app/pages/index.js
@@ -14,7 +14,6 @@ import Scanner from "../components/Scanner";
 import StudentBanner from "../components/StudentBanner";
 import Navbar from "../components/Navbar";
 import translations from "../translations/index.json";
-import { Helmet } from "react-helmet";
 
 export default function Home() {
   const [vw, setVw] = useState();
